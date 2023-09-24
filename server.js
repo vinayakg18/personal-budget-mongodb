fef01d24ef5984f0a44c20ae063aeabaa47cceb1
vinayakg18 <vinayakraddi18@gmail.com>
2023-09-24 16:01:23 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
+//app.use('/', express.static('public'));
 app.use('/jn',express.static('data.json'));
 
-app.get('/hello', (req,res) =>{
-    res.send('Hello World!');
-})
+//app.get('/hello', (req,res) =>{
+  //  res.send('Hello World!');
+//})
 
 app.get('/budget', (req, res) => {
     const url = 'http://localhost:3000/jn';
